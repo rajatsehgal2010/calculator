493c340c590d76508e04b06e37f827d4ffe77599
rajtron2010 <rajat.sehgal@crispanalytics.com>
2018-06-21 13:30:59 +0530

    Division and multiplication function are working bu using split function added in myfunc2()

--- script1.js
+++ script1.js
@@ -1,19 +1,20 @@
 
-	function myfunc1(){
+	function myfunc()
+	{
     var theInput = document.getElementById("display").value;
     
     theInput = theInput.substr(0, theInput.length - 1);
     document.getElementById("display").value=theInput;
-}
+    }
 
 function myfunc2()
 {
 	var a=document.getElementById("display").value;
 	var l=a.length;
 	var b=1;
-	for(i=0;i <l-1;i++)
+	for(j=0;j <l-1;j++)
 	{
-		if(a[i]=='/' && a[i+1]=='0')
+		if(a[j]=='/' && a[j+2]=='0')
 		{	
             b=0;
         }
@@ -24,21 +25,49 @@ if(b==0)
 else
 {
 	
+	str=a.split(" ");
+	var len=str.length;
+	
+	
+  if(len!=1){
+	for(i=1;i<len;i+=2)
+	{
+		var c;
+       if(str[i]=='/' || str[i]=='x')
+       {
+       	
+          if(str[i]=='/')
+          	{str[i-1]=parseInt(str[i-1])/parseInt(str[i+1]);}
+          else
+            {str[i-1]=parseInt(str[i-1])*parseInt(str[i+1]);}   
+       }
+       for(m=i;m<=len-3;m++)
+		{
+			str[m]=str[m+2];
+		}
+		len=len-2;
+	}
+  }
+}
+	
+	document.getElementById("display").value=str[0];
+
+}
+/*
+else
+{
+	
 
 var newString = a.split(" ");
-for(j=0;j<newString.length;j++)
-{
-	if(newString[j]!='+' && newString[j]!='-' && newString[j]!='/' &&newString[j]!='*')
-		newString[j]=parseInt(newString[j]);
-}
 var len=newString.length;
 for(len!=1)
 {
 	var c;
-	for(k=1;k<len;k=k+2)
+	for(k=1;k<len-1;k=k+2)
 	{
-		if(newString[k]=='/'){
-			c=newString[k-1]/newString[k+1];
+		if(newString[k]=='/')
+		{
+			c=parseInt(newString[k-1])/parseInt(newString[k+1]);
 		
 		newString[k-1]=c;
 		for(m=k;m<=len-3;m++)
@@ -46,8 +75,9 @@ for(len!=1)
 			newString[m]=newString[m+2];
 		}
 		len=len-2;
+	    }
 	}
-	}
+	/*
 	for(k=1;k<len;k=k+2)
 	{
 		if(newString[k]=='*'){
@@ -86,11 +116,14 @@ for(len!=1)
 		}
 		len=len-2;}
 	}
+	
 }
 document.getElementById("display").value=newString[0];
 
 }
 	//document.getElementById("display").value="WAIT.....";
 }
+
+*/
 
 
